9909e5e3e230caa219fc36c44163f377c4d4b70c
Jonny-Cho <andante2183@gmail.com>
2021-11-02 20:34:33 +0900

    step1: 프로젝트 생성

---
+++ src/main/java/hello/advanced/app/v0/OrderRepositoryVO.java
@@ -0,0 +1,25 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryVO {
+
+    public void save(final String itemId) {
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private void sleep(final int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
