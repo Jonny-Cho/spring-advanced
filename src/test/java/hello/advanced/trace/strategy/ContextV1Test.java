02d9eccdaa8a611a3e6b0915abe29dacc214acb7
Jonny-Cho <andante2183@gmail.com>
2021-11-05 22:01:18 +0900

    step3-2: 전략 패턴 시작

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,35 @@
+package hello.advanced.trace.strategy;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic1() {
+        final long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+    private void logic2() {
+        final long startTime = System.currentTimeMillis();
+        //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //종료
+        final long endTime = System.currentTimeMillis();
+        final long resultTime = endTime - startTime;
+        log.info("resultTime = {}", resultTime);
+    }
+
+}
